bc5a044a82afa72eb13b4e2a4046368353211de4
Moticob <oussama.ed-derouach@outlook.com>
2024-03-06 17:16:47 +0100

    up all

--- 0x00-ES6_basic/101-main.js
+++ 0x00-ES6_basic/101-main.js
@@ -1,7 +1,7 @@
-import createEmployeesObject from "../11-createEmployeesObject.js";
-import createReportObject from '../12-createReportObject.js';
-import createIteratorObject from '../100-createIteratorObject.js';
-import iterateThroughObject from '../101-iterateThroughObject.js';
+import createEmployeesObject from "./11-createEmployeesObject.js";
+import createReportObject from './12-createReportObject.js';
+import createIteratorObject from './100-createIteratorObject.js';
+import iterateThroughObject from './101-iterateThroughObject.js';
 
 
 const employees = {
@@ -13,3 +13,4 @@ const report = createReportObject(employees);
 const reportWithIterator = createIteratorObject(report);
 
 console.log(iterateThroughObject(reportWithIterator));
+
